d83a3756dfe1f180c145ad32c1562dadf7562a3e
rfloyd01 <lynrdskynrd05@gmail.com>
2022-11-10 10:46:56 -0500

    initial commit

---
+++ src/main/java/com/example/certrenew/CertRenewApplication.java
@@ -0,0 +1,13 @@
+package com.example.certrenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertRenewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertRenewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/certrenew/CertRenewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certrenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertRenewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
